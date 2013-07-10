fe0cec2a9a1b942f04d490df34505f26e940cc08
Graydon Armstrong <seeds77222@hotmail.com>
2013-07-10 18:27:06 -0400

    Initial Commit

---
+++ sidescrollerV1.py
@@ -0,0 +1,10 @@
+# Name: Graydon Armstrong
+# Date: July 10th, 2013
+# Source File: sidescrollerV1.py
+# Last Modified By: Graydon Armstrong
+# Date Last Modified: July 10th, 2013
+# Program description: A side scroller where you must get a high score
+# Revision History: Version 1 is making the basic framework
+
+#I - Import and initialize
+import pygame, random
