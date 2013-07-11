020ddaa0fdd09343e27b83443149cd0ab3747448
Graydon Armstrong <seeds77222@hotmail.com>
2013-07-10 20:50:12 -0400

    Created basic game loop with a temporary player object

--- sidescrollerV1.py
+++ sidescrollerV1.py
@@ -8,3 +8,51 @@
 
 #I - Import and initialize
 import pygame, random
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25,25))
+        self.image.fill((255,0,0))
+        self.image.convert()
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        mousex, mousey = pygame.mouse.get_pos()
+        self.rect.center = (mousex, mousey)
+
+def game():
+    pygame.display.set_caption("Game")
+    
+    background = pygame.Surface(screen.get_size())
+    background.fill((0,0,0))
+    screen.blit(background, (0,0))
+    
+    player = Player()
+    
+    freindSprites = pygame.sprite.Group(player)
+    
+    clock = pygame.time.Clock()
+    keepGoing = True
+    while keepGoing:
+        clock.tick(30)
+        pygame.mouse.set_visible(False)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+                
+        freindSprites.update()
+                
+        screen.blit(background, (0,0))
+        freindSprites.draw(screen)        
+        
+        pygame.display.flip()
+    
+def main():
+    game()
+    
+if __name__ == "__main__":
+    main()
