26993cd17c2523c1f792d9e03d010c8395f7755e
Graydon Armstrong <seeds77222@hotmail.com>
2013-07-11 19:59:16 -0400

    Forgot to change date

--- sidescrollerV2.py
+++ sidescrollerV2.py
@@ -1,8 +1,8 @@
 # Name: Graydon Armstrong
-# Date: July 10th, 2013
+# Date: July 11th, 2013
 # Source File: sidescrollerV1.py
 # Last Modified By: Graydon Armstrong
-# Date Last Modified: July 10th, 2013
+# Date Last Modified: July 11th, 2013
 # Program description: A side scroller where you must get a high score
 # Revision History: Version 2 is added final graphics to the game, sound, and final polish
 
